15d331e9932e4922d847065813e36474ab0f3310
weirdo-dot <africanweirdoblog@gmail.com>
2024-06-08 12:15:09 +0100

    dom

---
+++ src/UserList.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './UserList.css'; 
+
+const UserList = () => {
+  const [listOfUser, setListOfUser] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        setListOfUser(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the users!', error);
+      });
+  }, []);
+
+  return (
+    <div className="user-list">
+      <h1>User List</h1>
+      <ul>
+        {listOfUser.map(user => (
+          <li key={user.id} className="user-item">
+            <h2>{user.name}</h2>
+            <p>Email: {user.email}</p>
+            <p>Phone: {user.phone}</p>
+            <p>Website: {user.website}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserList;
